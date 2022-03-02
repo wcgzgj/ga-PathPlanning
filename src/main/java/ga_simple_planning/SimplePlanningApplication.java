4a6dc7bfe4d5abe4b9b65cf19a8431da409b6f72
wcgzgj <zhangjia_good@126.com>
2022-03-02 21:36:55 +0800

    初始类创建

---
+++ src/main/java/ga_simple_planning/SimplePlanningApplication.java
@@ -0,0 +1,14 @@
+package ga_simple_planning;
+
+/**
+ * @ClassName SImplePlanningApplication
+ * @Description TODO
+ * @Author faro_z
+ * @Date 2022/3/2 9:36 下午
+ * @Version 1.0
+ **/
+public class SimplePlanningApplication {
+    public static void main(String[] args) {
+
+    }
+}
