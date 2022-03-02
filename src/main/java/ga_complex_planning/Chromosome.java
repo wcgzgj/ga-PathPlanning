acb63d73729c5732490b6715b5b096cd26fdb46f
wcgzgj <zhangjia_good@126.com>
2022-03-02 21:37:48 +0800

    复杂路径规划初始类

---
+++ src/main/java/ga_complex_planning/Chromosome.java
@@ -0,0 +1,12 @@
+package ga_complex_planning;
+
+/**
+ * @ClassName Chromosome
+ * @Description TODO
+ * @Author faro_z
+ * @Date 2022/3/2 9:35 下午
+ * @Version 1.0
+ **/
+public class Chromosome {
+
+}
---
+++ src/main/java/ga_complex_planning/ComplexPlanningApplication.java
@@ -0,0 +1,14 @@
+package ga_complex_planning;
+
+/**
+ * @ClassName SImplePlanningApplication
+ * @Description TODO
+ * @Author faro_z
+ * @Date 2022/3/2 9:36 下午
+ * @Version 1.0
+ **/
+public class ComplexPlanningApplication {
+    public static void main(String[] args) {
+
+    }
+}
