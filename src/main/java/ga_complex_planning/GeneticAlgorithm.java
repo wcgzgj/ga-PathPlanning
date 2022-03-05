591582cded4809c1c89e76c5fc966c45e47cdd25
wcgzgj <zhangjia_good@126.com>
2022-03-05 20:29:07 +0800

    针对路径规划问题，修改了部分配置文件数据

--- src/main/java/ga_complex_planning/GeneticAlgorithm.java
+++ src/main/java/ga_complex_planning/GeneticAlgorithm.java
@@ -2,8 +2,12 @@ package ga_complex_planning;
 
 import ga_complex_planning.planning_info.Info;
 import ga_complex_planning.pojo.Point;
+import util.PropertyUtil;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * @ClassName GeneticAlgorithm
@@ -13,5 +17,70 @@ import java.util.Map;
  * @Version 1.0
  **/
 public class GeneticAlgorithm {
+    private Properties gaComplexPro = PropertyUtil.getProperty("ga_complex");
+    private Properties planningInfoPro = PropertyUtil.getProperty("planning_info");
 
+    // 受灾地信息
+    private Map<String, Point> info = Info.getInfo();
+
+    // 车辆数目
+    private int CAR_NUM = Integer.valueOf(planningInfoPro.getProperty("CAR_NUM"));
+    // 车辆载重
+    private int CAR_CAPACITY = Integer.valueOf(planningInfoPro.getProperty("CAR_CAPACITY"));
+    // 车辆速度
+    private int CAR_SPEED = Integer.valueOf(planningInfoPro.getProperty("CAR_SPEED"));
+    // 受灾点个数
+    private int POINT_NUM = info.size();
+
+    // 种群
+    private List<Chromosome> pop = new ArrayList<>();
+    // 种群大小
+    private int POP_SIZE = Integer.valueOf(gaComplexPro.getProperty("POP_SIZE"));
+    // 迭代次数
+    int ITER_NUM = Integer.valueOf(gaComplexPro.getProperty("ITER_NUM"));
+
+    // 基因长短
+    private int geneSize = calculateGeneSize();
+    // 当前种群最佳适应度
+    private double bestScore = Double.MIN_VALUE;
+    // 当前种群最坏适应度
+    private double worstScore = Double.MAX_VALUE;
+    // 当前种群总适应度
+    private double totalScore = 0;
+    // 当前种群平均适应度
+    private double averageScore = 0;
+    // 总迭代中的最佳基因
+    private int[] bestGene = null;
+    // 总迭代中的最坏基因
+    private int[] worstGene = null;
+
+    /**
+     * 执行遗传算法函数
+     */
+    public void conductGA() {
+        for (int i = 0; i < ITER_NUM; i++) {
+
+        }
+    }
+
+    /**
+     * 计算个体适应度
+     * 需要考虑：
+     * 1、单个车体载重能否满足所有受灾点
+     * 2、单个车体到达时间是否在时间窗之内
+     * @param chromosome
+     */
+    private void calculateScore(Chromosome chromosome) {
+
+    }
+
+    /**
+     * 计算基因长短
+     * 因为路径规划问题中，基因长短和车辆个数和受灾点有关系
+     * 所以需要额外开函数计算
+     * @return
+     */
+    private int calculateGeneSize() {
+        return CAR_NUM+POINT_NUM+1;
+    }
 }
--- src/main/java/ga_complex_planning/planning_info/Info.java
+++ src/main/java/ga_complex_planning/planning_info/Info.java
@@ -15,11 +15,11 @@ import java.util.Map;
 public class Info {
     private static Map<String, Point> info = new HashMap<>();
     static {
-        info.put("A",new Point("A",100d,100d,1.5d,0.78d,0d,7.1d));
-        info.put("B",new Point("B",100d,100d,1.5d,0.78d,0d,7.1d));
-        info.put("C",new Point("C",100d,100d,1.5d,0.78d,0d,7.1d));
-        info.put("D",new Point("D",100d,100d,1.5d,0.78d,0d,7.1d));
-        info.put("E",new Point("E",100d,100d,1.5d,0.78d,0d,7.1d));
+        info.put("A",new Point("A",160d,180d,0.6d,0.18d,0d,3.4d));
+        info.put("B",new Point("B",170d,50d,0.8d,0.48d,0d,7.1d));
+        info.put("C",new Point("C",30d,120d,1.3d,0.78d,0d,5.6d));
+        info.put("D",new Point("D",60d,90d,0.7d,1.08d,0d,3.2d));
+        info.put("E",new Point("E",250d,30d,1.3d,1.38d,0d,10d));
     }
 
     public static Map<String, Point> getInfo() {
